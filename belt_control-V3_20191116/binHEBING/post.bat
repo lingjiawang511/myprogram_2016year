f80da849bfa939dfd2591f7bb43cbe73e28e99b5
lingjiawang511 <398677675@qq.com>
2020-03-13 11:04:29 +0800

    add the create bin file and update the belt control project

--- belt_control-V3_20191116/binHEBING/post.bat
+++
@@ -1,4 +0,0 @@
-del ..\belt_control_link.bin
-fsutil  file createnew ..\belt_control_link.bin 16384
-..\binHEBING\cat ..\belt_control_app.bin >>..\belt_control_link.bin
-..\binHEBING\hbin ..\binHEBING\STM32F4xx_IAP.bin ..\belt_control_link.bin
